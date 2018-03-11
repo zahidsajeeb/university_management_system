6d36acca510f4bb2b5a5fa5deac4e65b746d03a1
unknown <zahidsajeeb100@gmail.com>
2018-03-12 05:06:25 +0600

    some changes

---
+++ resources/views/admin/student/edit.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: SAJEEB
+ * Date: 3/12/2018
+ * Time: 4:05 AM
+ */
---
+++ resources/views/admin/student/show.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: SAJEEB
+ * Date: 3/12/2018
+ * Time: 3:42 AM
+ */
